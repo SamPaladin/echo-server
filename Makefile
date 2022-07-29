a36656a81bd9f737fd0787925489fd9b1bd64826
Sam Alonso <sam@paladinstudios.com>
2022-07-29 14:50:15 +0200

    feat: Initial commit

---
+++ Makefile
@@ -0,0 +1,17 @@
+GO = CGO_ENABLED=0 go
+BINARY_NAME=analytics-server
+
+gen-protobuf:
+	protoc --proto_path=proto/ proto/*.proto --go_out=. --go-grpc_out=.
+
+build-osx: 
+	GOOS=darwin GOARCH=amd64 $(GO) build -o build/$(BINARY_NAME)-osx main.go
+
+build-windows:
+	GOARCH=386 GOOS=window go build -o ${BINARY_NAME}-windows main.go
+
+run-server:
+	./build/$(BINARY_NAME)-osx
+
+clean:
+	rm -rf build
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	pb "analytics/server/analytics/pb"
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	pb.UnimplementedRouteAnalyticsServiceServer
+}
+
+func (s *server) SessionStartedEvent(ctx context.Context, in *pb.SessionStartedEventRequest) (*pb.SessionStartedEventResponse, error) {
+	log.Printf("Received request: %v", in.ProtoReflect().Descriptor().FullName())
+	log.Printf("-------params Datetime: %v", in.Datetime)
+	log.Printf("-------params EventID: %v", in.EventId)
+	log.Printf("-------params IsNewUser: %v", in.NewUser)
+	log.Printf("-------params SessionCount: %v", in.SessionCount)
+	log.Printf("------- END MESSAGE")
+	return &pb.SessionStartedEventResponse{}, nil
+}
+
+func (s *server) StageCompletedEvent(ctx context.Context, in *pb.StageCompletedEventRequest) (*pb.StageCompletedEventResponse, error) {
+	log.Printf("Received request: %v", in.ProtoReflect().Descriptor().FullName())
+	log.Printf("-------params Datetime: %v", in.Datetime)
+	log.Printf("-------params EventID: %v", in.EventId)
+	log.Printf("-------params SessionCount: %v", in.SessionCount)
+	log.Printf("-------params ActiveDays: %v", in.ActiveDays)
+	log.Printf("-------params AllStars: %v", in.AllStars)
+	log.Printf("-------params Attempts: %v", in.Attempts)
+	log.Printf("-------params BasicCompletion: %v", in.BasicCompletion)
+	log.Printf("-------params BasicStars: %v", in.BasicStars)
+	log.Printf("-------params EventCompletion: %v", in.EventCompletion)
+	log.Printf("-------params EventFullCompletion: %v", in.EventFullCompletion)
+	log.Printf("-------params EventStamps: %v", in.EventStamps)
+	log.Printf("-------params FirstSuccess: %v", in.FirstSuccess)
+	log.Printf("-------params GameCompletion: %v", in.GameCompletion)
+	log.Printf("-------params GameMode: %v", in.GameMode)
+	log.Printf("-------params IsBonus: %v", in.IsBonus)
+	log.Printf("-------params MaxChapter: %v", in.MaxChapter)
+	log.Printf("-------params PrevAllStars: %v", in.PrevAllStars)
+	log.Printf("-------params PrevBasicStars: %v", in.PrevBasicStars)
+	log.Printf("-------params StageId: %v", in.StageId)
+	log.Printf("-------params StarsCompletion: %v", in.StarsCompletion)
+	log.Printf("------- END MESSAGE")
+
+	return &pb.StageCompletedEventResponse{}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Started Analytics server DEV environment")
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterRouteAnalyticsServiceServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
